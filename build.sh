ad7c6948ffbe5f1cd3916c4d30babaed066f226a
Josh Nichols <joshua.nichols@gmail.com>
2021-08-19 17:45:26 -0400

    Add commands for build and dev

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -xe
+
+bin_path=$(swift build --configuration release --show-bin-path --arch arm64 --arch x86_64)
+swift build --configuration release --arch arm64 --arch x86_64
+
+mkdir -p build
+cp alfred/* build/
+cp "${bin_path}/systemaudio" build/
+
+mkdir -p dist
+cd build
+zip -r ../dist/systemaudio.alfredworkflow *
---
+++ dev.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -xe
+swift build
+
+mkdir -p dev
+
+cd dev
+ln -sf ../alfred/* ./
+ln -sf ../.build/x86_64-apple-macosx/release/systemaudio .
+
+prefs=~/Library/Preferences/com.runningwithcrayons.Alfred-Preferences.plist
+syncfolder=$(/usr/libexec/PlistBuddy -c "print :syncfolder" $prefs 2>/dev/null || echo)
+if [ -z "$syncfolder" ]; then
+  syncfolder=~/Library/Application\ Support/Alfred
+fi
+
+workflows=$syncfolder/Alfred.alfredpreferences/workflows
+workflows=${workflows/\~/$HOME} # expand ~/
+
+if [ -d "$workflows" ]; then
+  echo "symlinking $(pwd) into $workflows"
+  ln -sf "$(pwd)" "$workflows/systemaudio"
+else
+  echo "Could not find workflows folder: $workflows"
+  exit 1
+fi
