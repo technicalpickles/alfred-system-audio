356a0f13846eb6815244e2374cda962a87e7a150
Josh Nichols <joshua.nichols@gmail.com>
2021-08-19 17:50:49 -0400

    try to fix release files

--- build.sh
+++ build.sh
@@ -4,10 +4,11 @@ set -xe
 bin_path=$(swift build --configuration release --show-bin-path --arch arm64 --arch x86_64)
 swift build --configuration release --arch arm64 --arch x86_64
 
-mkdir -p build
+mkdir -p build dist
+
 cp alfred/* build/
 cp "${bin_path}/systemaudio" build/
+cp "${bin_path}/systemaudio" dist/
 
-mkdir -p dist
 cd build
 zip -r ../dist/systemaudio.alfredworkflow *
